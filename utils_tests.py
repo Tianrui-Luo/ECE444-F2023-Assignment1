afb5027e9864cf4e564f71f60a4df476e8ab6b1b
Tianrui-Luo <77422312+Tianrui-Luo@users.noreply.github.com>
2023-09-13 22:01:31 -0400

    Create utils_tests.py

---
+++ utils_tests.py
@@ -0,0 +1,16 @@
+import utils
+
+print("Testing functions with integer:")
+print("Input: 714")
+print("Output from reversed function:", reversed(714))
+print("Output from formatter function:", formatter(714))
+
+print("Testing functions with strings:")
+print("Input: "Tianrui"")
+print("Output from reversed function:", reversed("Tianrui"))
+print("Output from formatter function:", formatter("Tianrui"))
+
+print("Testing functions with floats:")
+print("Input: 714.4")
+print("Output from reversed function:", reversed(714.4))
+print("Output from formatter function:", formatter(714.4))

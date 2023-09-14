a67e37213b9886a627303ad7e76606dc37fbac2c
Tianrui-Luo <77422312+Tianrui-Luo@users.noreply.github.com>
2023-09-13 21:59:52 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 class utils:
 
-def reversed(number): 
-  print(reverse(number))
+  def reversed(number):
+    num=str(number)
+    num_rev=int(num[len(num)::-1])
+    print(num_rev)
 
-def formatter(number):
-  print("Binary:",bin(number))
-  print("Octal:",oct(number))
+  def formatter(number):
+    print("Binary:",bin(number))
+    print("Octal:",oct(number))
